3e6b805b3b79ce0548271181f55fa9c01b9cc888
Riley Sumner <rsumner@wisc.edu>
2021-07-20 10:26:36 -0500

    update name

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Welcome to FFL Dashboard"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
