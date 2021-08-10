8956d87d90465fad8f9a152635cd477e8ef77008
Josh Sumner <Josh.Sumner@target.com>
2021-08-09 19:29:19 -0500

    fix file

--- app.R
+++ app.R
@@ -10,39 +10,28 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Welcome to FFL Dashboard"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  
+  titlePanel("RESE Draft Board - 2021"), 
+  selectInput("QB1", "Quarterbacks", c(players$Name, ""), ""),
+  selectInput("QB2", NULL, c(players$Name), ""),
+  selectInput("QB3", NULL, c(players$Name), ""),
+  selectInput("QB4", NULL, c(players$Name), ),
+  selectInput("RB1", "Running Backs", c(players$Name)),
+  selectInput("RB2", NULL, c(players$Name)),
+  selectInput("RB2", NULL, c(players$Name)),
+  selectInput("RB3", NULL, c(players$Name)),
+  selectInput("RB4", NULL, c(players$Name)),
+  selectInput("RB5", NULL, c(players$Name)),
 )
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  player_library = data.frame(players)
+  player_library
 }
 
 # Run the application 
