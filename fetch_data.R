9a2cf9aee97bfacea92be7a0709d55bcf4897f7f
Riley Sumner <rsumner@wisc.edu>
2021-08-26 16:40:03 -0500

    fix player name by id

--- fetch_data.R
+++ fetch_data.R
@@ -1,8 +1,8 @@
-players <- data.frame(read.csv("./data/player.csv", header = TRUE))
-rosters <- data.frame(read.csv("./data/fantasyteam.csv", header = TRUE))
-teams <- data.frame(read.csv("./data/teams.csv", header = TRUE))
-values <- data.frame(read.csv("./data/values.csv", header = TRUE))
-util <- data.frame(read.csv("./data/util.csv", header = TRUE))
+players <- as_tibble(data.frame(read.csv("./data/player.csv", header = TRUE)))
+rosters <- as_tibble(data.frame(read.csv("./data/fantasyteam.csv", header = TRUE)))
+teams <- as_tibble(data.frame(read.csv("./data/teams.csv", header = TRUE)))
+values <- as_tibble(data.frame(read.csv("./data/values.csv", header = TRUE)))
+util <- as_tibble(data.frame(read.csv("./data/util.csv", header = TRUE)))
 
 str(players)
 class(players)
--- utility.R
+++ utility.R
@@ -1,21 +1,21 @@
-get_roster_by_player_id <- function(id){
+get_roster_by_player_id <- function(id) {
   return(filter(rosters, player_id == id) %>%
            select(team_id))
 }
 
-get_full_roster_ids <- function(roster_id){
+get_full_roster_ids <- function(roster_id) {
   return(filter(rosters,team_id == roster_id) %>%
            select(player_id)) 
 }
 
-get_full_roster_names <- function(roster_id){
-  return(filter(rosters,team_id == roster_id) %>%
-           mutate(player_name = get_player_name_by_id(player_id)) %>%
-           select(player_name)) 
+get_full_roster_names <- function(roster_id) {
+  return(filter(rosters, team_id == roster_id) %>%
+           mutate(player_name = 'Tom Brady'))
 }
 
-get_player_name_by_id <- function(id){
-  return(filter(players, player_id == id) %>%
-           select(player_name)[1])
+get_player_name_by_id <- function(id) {
+  df = filter(players, player_id == id) %>%
+    select(player_name)
+  return(df$player_name)
 }
 
