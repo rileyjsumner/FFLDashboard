223489f6490a8a4dc64e438a31313111c35cb337
Josh Sumner <Josh.Sumner@target.com>
2021-08-26 17:53:52 -0500

    added functions

--- utility.R
+++ utility.R
@@ -9,8 +9,15 @@ get_full_roster_ids <- function(roster_id) {
 }
 
 get_full_roster_names <- function(roster_id) {
-  return(filter(rosters, team_id == roster_id) %>%
-           mutate(player_name = 'Tom Brady'))
+  df = get_full_roster_ids(roster_id)
+  return(merge(players,df,all.x = FALSE) %>%
+           select(player_id,player_name))
+}
+
+get_full_roster_values <- function(roster_id) {
+  df = get_full_roster_ids(roster_id)
+  return(merge(values,df,all.x = FALSE) %>%
+           select(player_id,value))
 }
 
 get_player_name_by_id <- function(id) {
@@ -19,3 +26,15 @@ get_player_name_by_id <- function(id) {
   return(df$player_name)
 }
 
+display_full_roster <- function(id){
+  df1 = get_full_roster_names(id)
+  df2 = get_full_roster_values(id)
+  return(merge(df1,df2))
+}
+
+get_full_roster_data <- function(id){
+  df1 = get_full_roster_ids(id)
+  df2 = merge(players,df1,all.x = FALSE)
+  df3 = get_full_roster_values(id)
+  return(merge(df2,df3))
+}
