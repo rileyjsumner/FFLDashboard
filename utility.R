0c1641356de38b41730433d61bbb5eb5aedff01c
Josh Sumner <Josh.Sumner@target.com>
2021-08-26 16:19:44 -0500

    added funtions

--- utility.R
+++ utility.R
@@ -1,4 +1,21 @@
-get_roster_by_player_id <- function(player_id){
-  return(rosters$team_id[rosters$player_id == player_id])
+get_roster_by_player_id <- function(id){
+  return(filter(rosters, player_id == id) %>%
+           select(team_id))
+}
+
+get_full_roster_ids <- function(roster_id){
+  return(filter(rosters,team_id == roster_id) %>%
+           select(player_id)) 
+}
+
+get_full_roster_names <- function(roster_id){
+  return(filter(rosters,team_id == roster_id) %>%
+           mutate(player_name = get_player_name_by_id(player_id)) %>%
+           select(player_name)) 
+}
+
+get_player_name_by_id <- function(id){
+  return(filter(players, player_id == id) %>%
+           select(player_name)[1])
 }
 
